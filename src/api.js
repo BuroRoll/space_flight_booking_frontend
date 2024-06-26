7a6e43b4afd45f1508e1834a45c2f1a27a6f34f1
Danil <BuroRoll@gmail.com>
2024-06-26 16:46:28 +0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://buroroll-spaceflightbooking-494b.twc1.net/docs';
 
 const api = axios.create({
     baseURL: API_URL,
