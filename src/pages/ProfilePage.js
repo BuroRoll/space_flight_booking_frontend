516ecd75dcb204ee53140f9dd32066dd4c283630
Danil <BuroRoll@gmail.com>
2024-06-27 15:54:08 +0500

    Update ProfilePage.js

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -5,13 +5,18 @@ import SimpleFlightCard from "../components/SimpleFlightCard";
 
 const ProfilePage = () => {
     const [activeSection, setActiveSection] = useState('information');
-    const [user, setUser] = useState({name: '', email:'', surname: ''})
+    const [user, setUser] = useState({name: '', email: '', surname: ''})
+    const [name, setName] = useState(user.name);
+    const [surname, setSurname] = useState(user.surname);
+    const [email, setEmail] = useState(user.email);
 
     useEffect(() => {
         const getUserData = async () => {
             try {
                 const user = await getUserInfo();
-                console.log(user)
+                setName(user.name)
+                setSurname(user.surname)
+                setEmail(user.email)
                 setUser(user);
             } catch (error) {
                 console.error("Failed to fetch user data", error);
@@ -19,6 +24,11 @@ const ProfilePage = () => {
         };
         getUserData();
     }, []);
+
+    const handleSave = () => {
+        updateUser(name, surname, email);
+    };
+
     const renderSection = () => {
         switch (activeSection) {
             case 'information':
@@ -26,16 +36,29 @@ const ProfilePage = () => {
                     <div className="profile-info">
                         <div className="profile-item">
                             <label>Имя:</label>
-                            <span>{user.name}</span>
+                            <input
+                                type="text"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
                         </div>
                         <div className="profile-item">
                             <label>Фамилия:</label>
-                            <span>{user.surname}</span>
+                            <input
+                                type="text"
+                                value={surname}
+                                onChange={(e) => setSurname(e.target.value)}
+                            />
                         </div>
                         <div className="profile-item">
                             <label>Электронная почта:</label>
-                            <span>{user.email}</span>
+                            <input
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
                         </div>
+                        <button className="profile-button" onClick={handleSave}>Обновить</button>
                     </div>
                 );
             case 'bookings':
